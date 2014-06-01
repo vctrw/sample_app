e6b2cda75ac7854e9b5798eea5449cc72ab12c42
Victor <wangvictorh@gmail.com>
2014-06-01 15:21:12 -0400

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,10 @@ SampleApp::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
